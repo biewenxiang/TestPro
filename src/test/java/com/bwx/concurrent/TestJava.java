8c135ab071b12020ea504435a06f86b537f19ef2
biewx <biewenxiang>
2020-05-06 15:14:19 +0800

    v1.0test

--- src/test/java/com/bwx/concurrent/TestJava.java
+++ src/test/java/com/bwx/concurrent/TestJava.java
@@ -1,7 +1,7 @@
 package com.bwx.concurrent;
 
 public class TestJava {
-    public void testV1(){
-        System.out.println("test11");
+    public void testv1(){
+        System.out.println(1111);
     }
 }
