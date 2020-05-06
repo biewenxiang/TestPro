99840d6422897d02c06aa8186ad680475d08492f
biewx <biewenxiang>
2020-05-06 15:51:28 +0800

    Merge branch 'testV1.0' into testV1.1
    # Conflicts:
    #	src/test/java/com/bwx/concurrent/TestJava.java

--- src/test/java/com/bwx/concurrent/TestJava.java
+++ src/test/java/com/bwx/concurrent/TestJava.java
@@ -3,10 +3,13 @@ package com.bwx.concurrent;
 public class TestJava {
     public void testV1() {
         System.out.println("test11");
-    public void testv1(){
+    }
+
+    public void testv1() {
         System.out.println(1111);
     }
-    public void testv1_1(){
+
+    public void testv1_1() {
         System.out.println("v1_1");
     }
 }
