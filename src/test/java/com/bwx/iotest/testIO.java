635deea74dbbbfebae523e85e723c2aa391eb6e8
biewx <biewenxiang>
2020-12-28 15:26:30 +0800

    devtest

--- src/test/java/com/bwx/iotest/testIO.java
+++ src/test/java/com/bwx/iotest/testIO.java
@@ -13,6 +13,11 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class testIO {
 
 
+    /**
+     * 字节流测试
+     *
+     * @throws IOException
+     */
     @Test
     public void testIo() throws IOException {
         //读取文件(字节流)
@@ -21,13 +26,13 @@ public class testIO {
         OutputStream out = new FileOutputStream("d:\\test\\2.txt");
         //读取数据
         //一次性取多少字节
-        byte[] bytes = new byte[2048];
+        byte[] bytes = new byte[2];
         //接受读取的内容(n就代表的相关数据，只不过是数字的形式)
         int n = -1;
         //循环取出数据
         while ((n = in.read(bytes, 0, bytes.length)) != -1) {
             //转换成字符串
-            String str = new String(bytes, 0, n, "GBK");//这里可以实现字节到字符串的转换，比较实用
+            String str = new String(bytes, 0, n, "Utf-8");//这里可以实现字节到字符串的转换，比较实用
             System.out.println(str);
             //写入相关文件
             out.write(bytes, 0, n);
@@ -37,7 +42,7 @@ public class testIO {
         out.close();
     }
 
-    //缓存字符流
+    //缓存字节流
     @Test
     public void buffTest() throws IOException {
         //读取文件(缓存字节流)
@@ -46,13 +51,15 @@ public class testIO {
         BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream("d:\\test\\2.txt"));
         //读取数据
         //一次性取多少字节
-        byte[] bytes = new byte[2048];
+//        byte[] bytes = new byte[2048];
+        byte[] bytes = new byte[2];
+
         //接受读取的内容(n就代表的相关数据，只不过是数字的形式)
         int n = -1;
         //循环取出数据
         while ((n = in.read(bytes, 0, bytes.length)) != -1) {
             //转换成字符串
-            String str = new String(bytes, 0, n, "GBK");
+            String str = new String(bytes, 0, n, "UTF-8");
             System.out.println(str);
             //写入相关文件
             out.write(bytes, 0, n);
@@ -67,11 +74,11 @@ public class testIO {
     @Test
     public void testReader() throws IOException {
         //读取文件(字符流)
-        BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream("d:\\test\\test.txt"), "GBK"));
+//        BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream("d:\\test\\test.txt"), "UTF-8"));
         //#这里主要是涉及中文
-//        BufferedReader in = new BufferedReader(new FileReader("d:\\test\\test.txt"));
+        BufferedReader in = new BufferedReader(new FileReader("d:\\test\\test.txt"));
         //写入相应的文件
-        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("d:\\test\\2.txt"), "GBK"));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("d:\\test\\2.txt"), "UTF-8"));
         //BufferedWriter out = new BufferedWriter(new FileWriter("d:\\2.txt"))；
         //读取数据
         //循环取出数据
@@ -110,26 +117,27 @@ public class testIO {
     public void testio2() throws IOException {
         StringBuilder aaa = new StringBuilder();
         String string = "";
-        BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\admin\\Desktop\\aa.txt"), "UTF-8"));
+        BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\admin\\Desktop\\station.txt"), "UTF-8"));
         Set list = new HashSet();
         while ((string = in.readLine()) != null) {
-            if (string.contains("{\"") && string.contains("2019041708")) {
+//            if (string.contains("{\"") && string.contains("2019041708")) {
                 aaa.append(string);
-                string.split(",");
-                list.add(string.split(",")[0]);
-            }
+//                string.split(",");
+//                list.add(string.split(",")[0]);
+//            }
             //写入相关文件
         }
-        writeFile("C:\\Users\\admin\\Desktop\\aa2.txt", aaa.toString());
+//        writeFile("C:\\Users\\admin\\Desktop\\aa2.txt", aaa.toString());
 
         long begin = System.currentTimeMillis();
-//        for (int i = 1; i < 10000; i++) {
-//            writeFile("E:\\Test\\test1111\\test" + i + ".htm", aaa.toString());
-//        }
+        for (int i = 1; i < 10000; i++) {
+            writeFile4("E:\\Test\\test1111\\test" + i + ".htm", aaa.toString());
+        }
 //        7002; 5926  113,301 66,068
         System.out.println(System.currentTimeMillis() - begin);
 
     }
+    //21483字节流写入
 
     public static boolean writeFile(String filePath, String content) {
 
@@ -172,7 +180,7 @@ public class testIO {
         }
         return writeFlag;
     }
-
+//9431 字符流
     private boolean writeFile2(String filePath, String content) {
         File file = new File(filePath);
         boolean writeFlag = false;
@@ -211,7 +219,7 @@ public class testIO {
         return writeFlag;
     }
 
-
+//16837缓存字节流
     private void writeFile3(String filePath, String content) {
         File file = new File(filePath);
 
@@ -220,7 +228,6 @@ public class testIO {
         }
         FileOutputStream fos = null;
         BufferedOutputStream bos = null;
-
         try {
             fos = new FileOutputStream(filePath);
             bos = new BufferedOutputStream(fos);
@@ -252,50 +259,43 @@ public class testIO {
 
     }
 
-    @Test
-    public void iotest() {
-        String aa = "a";
-        ExecutorService executorService = Executors.newFixedThreadPool(4);
-        long start = System.currentTimeMillis();
+    //17708缓存字符流
+    private void writeFile4(String filePath, String content) {
+        File file = new File(filePath);
 
-        for (int i = 1; i < 50000; i++) {
-//            for (int i2 =1;i2<10;i++)
-            int finalI = i;
-//            2:18053 86370
-//            1:19497 82808
-//            3:17043 76750
+        if (!file.getParentFile().exists()) {
+            file.getParentFile().mkdirs();
+        }
+        FileWriter fos = null;
+        BufferedWriter bos = null;
+        try {
+            fos = new FileWriter(filePath);
+            bos = new BufferedWriter(fos);
+//            String contentStr = content.replaceAll("\\s+<", "\n<").replaceAll(">\\s+", ">\n").trim();
+            String contentStr = content.replaceAll("(?<=>)\\s+|\\s+(?=<)", "\n").trim();
 
-            Runnable worker = new Runnable() {
-                @Override
-                public void run() {
-                    writeFile("/ser/iotest/1" + finalI + ".txt", aa);
-
+//            byte[] tempBytes = contentStr.getBytes("UTF-8");
+            bos.write(contentStr);
+            bos.flush();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            if (bos != null) {
+                try {
+                    bos.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
                 }
-            };
-            executorService.submit(worker);
-
+            }
         }
-        executorService.shutdown();
-        while (!executorService.isTerminated()) {
-
-        }
-        System.out.println("Finished all threads");
-        System.out.println(aa);
-        System.out.println(System.currentTimeMillis() - start);
-        System.out.println("end");
-    }
-
-
-    @Test
-    public void testFileauth() throws IOException {
 
     }
-
-    @Test
-    public void testIO() throws IOException {
-        byte[] aa =  new byte[1024];
-
-
-    }
-
 }
